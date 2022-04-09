ad93263fec75ee140b681d6965e6ce865b9b976f
IbraheemShaban <ishaban097@gmail.com>
2022-04-09 03:04:50 +0300

    ADDED styling

--- components/taskItem.js
+++ components/taskItem.js
@@ -2,16 +2,19 @@ function tasksItemComponent(task) {
   // You are going to add your HTML design here.
   // Do the option design in HTML page, test it, and replace it if you need instead of this code.
   // For any variables, just use the interpolation operation ${} and pass the variable
-  const checked = task.done ? "checked" : "";
+  const checked = task.done ? 'checked' : '';
   return `
               <div class="todo-item">
+              <label class="container">
                 <input 
-                  class="task-checkbox" 
+                  class="checkbox" 
                   type="checkbox"
                   ${checked} 
                   onchange="taskChecked(${task.id}, this.checked)"
                 />
-                <div className="movie-details">
+                <span class="checkmark"></span>
+                </label>
+                <div class="movie-details">
                   <p class="item-category">${task.category}</p>
                   <p class="item-title">${task.title}</p>
                 </div>
